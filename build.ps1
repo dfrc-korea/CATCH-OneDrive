f7dc430b966c9334fef2a79a27fe7e2e9ce07411
DFRC <40653523+dfrc-korea@users.noreply.github.com>
2022-07-09 15:15:05 +0900

    Add files via upload

---
+++ build.ps1
@@ -0,0 +1,8 @@
+# CATCH Internal - OneDrive
+# version 20220709
+
+python -m venv ./venv
+.\venv\Scripts\activate
+
+.\venv\Scripts\pip install -r requirements.txt
+.\venv\Scripts\python -m playwright install
